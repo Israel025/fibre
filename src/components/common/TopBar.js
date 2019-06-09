44d0f345ba056a2c92131b81661021d7f082c367
Israel <israeltademola@gmail.com>
2019-06-09 06:00:30 +0100

    setting up

---
+++ src/components/common/TopBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class TopBar extends React.Component {
+  
+  
+  render() {
+    return (
+     <header>
+       <div>
+        <a href="#">Fibre</a>
+       </div>
+       <div>
+         
+       </div>
+     </header>
+    );
+  }
+}
+export default TopBar;
